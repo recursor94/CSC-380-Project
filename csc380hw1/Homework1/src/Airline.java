0d6694f69fd83ff517aa82825f49a1e343f869fe
Nguyen <9phuc.nguyen6@gmail.com>
2018-02-09 15:43:04 -0500

    created proper Airline.java class

---
+++ csc380hw1/Homework1/src/Airline.java
@@ -0,0 +1,17 @@
+public class Airline {
+	
+	private String name;
+	//private ArrayList<Planes> planeList;
+	//private Schedule schedule;
+	
+//	public void addSchedule(params) {
+//		schedule.add();
+//	}
+	
+	// Schedule will have plane, times, destination, arrival, flight
+
+}
+
+
+// CHEESE BURGER ASCII HERE
+//plane has max person(s) (capacity)
