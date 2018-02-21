b879d0441f87f51f181aa3f214b4774c4d6e111d
zgudlin <timberwolves77@gmail.com>
2018-02-21 15:42:35 -0500

    I created a user class

---
+++ csc380hw1/Homework1/src/User.java
@@ -0,0 +1,16 @@
+
+public class User {
+	
+	private String userName;
+	private String password;
+	
+	
+	//Constructor for a user's login ID and password
+	public User (String name, String pw) {
+		
+		userName = name;
+		password = pw;
+		
+	}
+
+}
