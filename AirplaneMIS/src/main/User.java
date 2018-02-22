e44bd29ea7eba9c30baef434f3fb6f9986bdf5f0
Nguyen <9phuc.nguyen6@gmail.com>
2018-02-22 13:28:46 -0500

    user management class

--- AirplaneMIS/src/main/User.java
+++ AirplaneMIS/src/main/User.java
@@ -5,15 +5,11 @@ public class User {
 	private String password;
 	
 	
-	//Constructor for a user's login ID and password
 	public User (String name, String pw) {
-		
 		userName = name;
 		password = pw;
-		
 	}
-	
-	//This is just a test method so I can run something in the Driver; we can delete this later
+
 	public void displayUser() {
 		
 		System.out.println("Login ID: " + userName +"\nPassword: " + password);
