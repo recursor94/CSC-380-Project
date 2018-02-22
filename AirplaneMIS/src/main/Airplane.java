6088e292146c247c86a7605a1f45164377afa369
zgudlin <timberwolves77@gmail.com>
2018-02-22 13:32:24 -0500

    I am submitting the Airplane class with updates

--- AirplaneMIS/src/main/Airplane.java
+++ AirplaneMIS/src/main/Airplane.java
@@ -6,9 +6,27 @@
  */
 
 public class Airplane {
-
 	private String name;
+	private int maxPassengers;
 	private int planeId;
 	private int[] seating;
-
+	private ArrayList<User> users;
+	private int numUsers;
+	
+	//Constructor - Probably needs a few more things in it, but I just wanted to get a start
+	public Airplane(int max) {
+		
+		numUsers = 0;
+		maxPassengers = max;
+		users = new ArrayList<User>(max);
+	}
+	
+	//Method to add a user to the user array list
+	public void addUser(User obj) {
+		
+		users.add(numUsers, obj);
+		numUsers ++;
+		
+	}
+	
 }
