a4f2f545b8393ebd0decc86dd2cd5edb25513a87
zgudlin <timberwolves77@gmail.com>
2018-02-22 14:09:01 -0500

    Added a constructor with seating and plane name

--- AirplaneMIS/src/main/Airplane.java
+++ AirplaneMIS/src/main/Airplane.java
@@ -3,5 +3,13 @@ package main;
 public class Airplane {
 	
 	private String name;
+	private Seating seats;
+	
+	public Airplane(String name, Seating obj) {
+		
+		this.name = name;
+		seats = new Seating obj;
+		
+	}
 
 }
