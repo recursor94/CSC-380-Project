45afb113477b8c16012ed634ab06db970b6bd9c8
zgudlin <timberwolves77@gmail.com>
2018-02-23 15:54:53 -0500

    Updated Driver

--- AirplaneMIS/src/main/Driver.java
+++ AirplaneMIS/src/main/Driver.java
@@ -1,10 +1,21 @@
 package main;
 
-public class Driver {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
 
 	public static void main(String[] args) {
 		User user = new User("Bob Saget", "helloMum");
 		System.out.println("User created! " + user);
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		stage.show();
+		
 	}
 
 }
