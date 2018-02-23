c9c296b44446eb8c7d24d018f9fb90a750447e54
zgudlin <timberwolves77@gmail.com>
2018-02-23 15:13:50 -0500

    Deleted a test file

--- AirplaneMIS/src/main/Test.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Test {
-
-	public static void main(String[] args) {
-		User user = new User("Bob Saget", "helloMum");
-		System.out.println("User created! " + user);
-	}
-}
