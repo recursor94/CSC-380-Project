a54306009a06effad8e5dcf189bedc236d1ff2df
Andrew Spano <spana75@mai.sunysuffolk.edu>
2018-03-22 13:47:28 -0400

    Created Unformatted findflightview

--- Workspace/airplane/src/main/java/apz/airplane/gui/FlightBookingFindView.java
+++ Workspace/airplane/src/main/java/apz/airplane/gui/FlightBookingFindView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,12 +14,19 @@ public class FlightBookingFindView extends Application{
 	public static final int FRAME_WIDTH = 450;
 	public static final int FRAME_HEIGHT = 350;
 	public static final String FRAME_TITLE = "Find Flights";
+	public static final String LABEL_PICK_DATE_NAME = "Choose A Date:";
+	public static final String LABEL_CHOOSE_DESTINATION_NAME = "Choose Destination: ";
+	public static final String BUTTON_CONFIRM_FLIGHT_NAME = "Confirm Flight";
+
 	private Stage primaryStage;
 	private VBox rootPane;
+	private HBox datePane;
+	private HBox destinationPane;
 	
 	private Label labelPickDate;
 	private Label labelChooseDestination;
 	
+	private Button buttonConfirm;
 	private DatePicker datePickerFlightDate;
 	private ComboBox comboBoxDestinationList;
 	
@@ -35,10 +43,19 @@ public class FlightBookingFindView extends Application{
 	
 	public void init() {
 		rootPane = new VBox(10);
+		datePane = new HBox(10);
+		destinationPane = new HBox(10);
+		labelPickDate = new Label(LABEL_PICK_DATE_NAME);
+		labelChooseDestination = new Label(LABEL_CHOOSE_DESTINATION_NAME);
+		datePickerFlightDate = new DatePicker();
+		comboBoxDestinationList = new ComboBox<>();
+		buttonConfirm = new Button(BUTTON_CONFIRM_FLIGHT_NAME);
 	}
 	
 	public void initContent() {
-		rootPane.getChildren().addAll(buttonFindFlight, buttonViewFlights);
+		datePane.getChildren().addAll(labelPickDate, datePickerFlightDate);
+		destinationPane.getChildren().addAll(labelChooseDestination, comboBoxDestinationList);
+		rootPane.getChildren().addAll(datePane, destinationPane, buttonConfirm);
 	}
 	public void initActionEvents() {
 		
