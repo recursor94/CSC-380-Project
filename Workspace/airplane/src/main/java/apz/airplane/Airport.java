48854198c818d60774d815ac0945ffd65ff10230
Andrew Spano <spana75@mai.sunysuffolk.edu>
2018-03-31 14:55:55 -0400

    added Airport class

---
+++ Workspace/airplane/src/main/java/apz/airplane/Airport.java
@@ -0,0 +1,31 @@
+package apz.airplane;
+
+public class Airport {
+	private String name;
+	private String city;
+	
+	public Airport(String name, String city) {
+		this.name = name;
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String toString() {
+		return "Airport name:\t" + getName() + "Airport city:\t" + getCity();
+	}
+}
