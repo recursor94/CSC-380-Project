f0395b82c3ca159f4ef5fcd4dec904129e28e9ca
Nguyen <9phuc.nguyen6@gmail.com>
2018-04-10 13:34:34 -0400

    Small fixes

--- Workspace/airplane/src/main/java/apz/airplane/gui/BookFlightByDateWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/gui/BookFlightByDateWindow.java
@@ -21,71 +21,70 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class BookFlightByDateWindow {
-	private VBox mainPane = new VBox(10);
-	private ListView<Flight> flights = new ListView <>();
-	private ArrayList <Flight> flightList = new ArrayList<>();
+	private VBox mainPane;
+	private ListView<Flight> flightView;
+	private ArrayList<Flight> flightList;
 	private DatePicker calendar;
 	private Button findFlightButton, bookFlightButton;
-	
-	
+
 	public BookFlightByDateWindow() {
 		initialize();
 		content();
 		actionEvents();
 	}
-	
+
 	private void initialize() {
+		mainPane = new VBox(10);
+		flightView = new ListView<>();
+		flightList = new ArrayList<>();
 		findFlightButton = new Button("Find Flights");
 		bookFlightButton = new Button("Book Flight");
-	    calendar = new DatePicker();
-	    calendar.setEditable(false);
+		calendar = new DatePicker();
+		calendar.setEditable(false);
 	}
-	
+
 	private void content() {
-		mainPane.getChildren().addAll(new Label("Select a flight date"), calendar, findFlightButton, 
-				new Label("List of flights on selected date"), flights, bookFlightButton);
+		mainPane.getChildren().addAll(new Label("Select a flight date"), calendar, findFlightButton,
+				new Label("List of flightView on selected date"), flightView, bookFlightButton);
 		APZLauncher.getBorderPane().setCenter(mainPane);
 	}
-	
+
 	private void actionEvents() {
 		findFlightButton.setOnAction(event -> {
 			if (calendar.getValue() != null) {
 				flightList = findFlights(calendar.getValue());
-				
-				if (!flights.getItems().isEmpty())
-					flights.getItems().clear();
+
+				if (!flightView.getItems().isEmpty())
+					flightView.getItems().clear();
 				for (int i = 0; i < flightList.size(); i++)
-					flights.getItems().add(flightList.get(i));
-				
-				if (flights.getItems().isEmpty()) {
-					MessageBox.message(AlertType.INFORMATION, "No Flights Found", "There are no flights scheduled for " + calendar.getValue());
-				}
-			}
-			else {
+					flightView.getItems().add(flightList.get(i));
+
+				if (flightView.getItems().isEmpty())
+					MessageBox.message(AlertType.INFORMATION, "No Flights Found", "There are no flightView scheduled for " + calendar.getValue());
+
+			} else 
 				MessageBox.message(AlertType.ERROR, "ERROR", "You must select a date");
-			}
 		});
-		
+
 		bookFlightButton.setOnAction(event -> {
-			if (!flights.getSelectionModel().isEmpty()) {
+			if (!flightView.getSelectionModel().isEmpty()) {
 				User user = APZLauncher.getCurrentUser();
-				user.addTrip(new Booking(flights.getSelectionModel().getSelectedItem(), LocalDate.now(), user));
+				user.addTrip(new Booking(flightView.getSelectionModel().getSelectedItem(), LocalDate.now(), user));
 				MessageBox.message(AlertType.INFORMATION, "Successful Booking", "Your flight has been booked!");
 				apz.airplane.util.State.saveInformation(APZLauncher.getUserController());
 				System.out.println(user.getTripList());
-			}
-			else {
+			} else {
 				MessageBox.message(AlertType.INFORMATION, "No Flight Selected", "You must select a flight to book");
 			}
 		});
 	}
 
 	private ArrayList<Flight> findFlights(LocalDate departure) {
-		
+
 		ArrayList<Flight> searchFlights = AdminState.loadFlights();
 		ArrayList<Flight> flightsFound = new ArrayList<>();
-		
-		for (int i = 0; i < searchFlights.size(); i ++) {
+
+		for (int i = 0; i < searchFlights.size(); i++) {
 			if (departure.equals(searchFlights.get(i).getDepartureDate())) {
 				flightsFound.add(searchFlights.get(i));
 			}
