44f381ab2ea84943d3b5fdd294c772475c440c41
Andrew Spano <werdnaonaps@gmail.com>
2018-04-12 12:44:35 -0400

    Added real time clock to home screen window

--- Workspace/airplane/src/main/java/apz/airplane/gui/HomeScreenWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/gui/HomeScreenWindow.java
@@ -1,21 +1,28 @@
 package apz.airplane.gui;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import apz.airplane.Flight;
 import apz.airplane.admin.AdminState;
 import apz.airplane.util.State;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class HomeScreenWindow {
 	private Button logoutButton;
@@ -24,6 +31,12 @@ public class HomeScreenWindow {
 	private Stage primaryStage;
 	private ListView activeFlightView;
 	private ComboBox cityComboBox;
+	private Label timeLabel;
+	private Timeline realTimeClock;
+	
+	private int timeHour;
+	private int timeMinute;
+	private int timeSecond;
 
 	public HomeScreenWindow() {
 		initialize();
@@ -40,7 +53,8 @@ public class HomeScreenWindow {
 		
 		ObservableList<Flight> flights = FXCollections.observableArrayList(getFlightsToday());
 		activeFlightView.setItems(flights);
-		rootPane.getChildren().addAll(activeFlightView);
+		timeLabel.setText(LocalDateTime.now().toString());
+		rootPane.getChildren().addAll(timeLabel, activeFlightView);
 		APZLauncher.getBorderPane().setCenter(rootPane);
 	
 	}
@@ -48,6 +62,28 @@ public class HomeScreenWindow {
 	private void initialize() {
 		rootPane = new VBox(10);
 		activeFlightView = new ListView<String>();
+		timeLabel = new Label("00:00");
+		
+		  realTimeClock = new Timeline(new KeyFrame(Duration.ZERO, e -> {            
+		        Calendar cal = Calendar.getInstance();
+		        timeSecond = cal.get(Calendar.SECOND);
+		        timeMinute = cal.get(Calendar.MINUTE);
+		        timeHour = cal.get(Calendar.HOUR);
+		        
+		        if(timeHour >= 12) {
+		        	timeHour -=12;
+		        }
+		        else if(timeHour == 0) {
+		        	timeHour = 12;
+		        }
+		        System.out.println(cal.get(Calendar.HOUR));
+		        //System.out.println(hour + ":" + (minute) + ":" + second);
+		        timeLabel.setText(timeHour + ":" + (timeMinute) + ":" + timeSecond);
+		    }),
+		         new KeyFrame(Duration.seconds(1))
+		    );
+		    realTimeClock.setCycleCount(Animation.INDEFINITE);
+		    realTimeClock.play();
 	}
 	
 	private ArrayList<Flight> getFlightsToday() {
@@ -60,5 +96,18 @@ public class HomeScreenWindow {
 		}
 		return flightsToday;
 	}
+	/*private void orderFlightsByTime(ArrayList<Flight> flightsToday) {
+		Flight lastAddedFlight = flightsToday.get(0);
+		ArrayList<Flight> orderedFlights = new ArrayList<>();
+		for(int i = 0; i < flightsToday.size(); i++) {
+			for(int j = 0; j < flightsToday.size(); j++) {
+				
+			}
+			
+		} 
+			
+		}*/ 
+		
+	}
 
-}
+
