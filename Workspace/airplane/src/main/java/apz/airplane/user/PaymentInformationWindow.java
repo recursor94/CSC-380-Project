5091be2908faf6adcb2af8345dc4227b23e4913f
zgudlin <timberwolves77@gmail.com>
2018-04-13 13:34:52 -0400

    I added state abbreviations to the combo box in PaymentInformationWindow, and I changed some various methods from public to private b/c they did not need to be public

--- Workspace/airplane/src/main/java/apz/airplane/user/PaymentInformationWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/PaymentInformationWindow.java
@@ -20,7 +20,7 @@ public class PaymentInformationWindow {
 		properties();
 	}
 	
-	public void initialize() {
+	private void initialize() {
 		rootPane = new GridPane();
 		cardNumField = new TextField();
 		nameField = new TextField();
@@ -31,7 +31,9 @@ public class PaymentInformationWindow {
 		submitButton = new Button("Submit");
 	}
 	
-	public void content() {
+	private void content() {
+		populateStates();
+		
 		rootPane.setHgap(10);
 		rootPane.setVgap(10);
 		
@@ -61,5 +63,58 @@ public class PaymentInformationWindow {
 		APZLauncher.getBorderPane().setCenter(rootPane);
 		APZLauncher.getStage().setHeight(425);
 		APZLauncher.getStage().setWidth(500);
+	}
+	
+	private void populateStates() {
+		stateBox.getItems().add("AL");
+		stateBox.getItems().add("AK");
+		stateBox.getItems().add("AR");
+		stateBox.getItems().add("AZ");
+		stateBox.getItems().add("CA");
+		stateBox.getItems().add("CO");
+		stateBox.getItems().add("CT");
+		stateBox.getItems().add("DE");
+		stateBox.getItems().add("FL");
+		stateBox.getItems().add("GA");
+		stateBox.getItems().add("HI");
+		stateBox.getItems().add("IA");
+		stateBox.getItems().add("ID");
+		stateBox.getItems().add("IL");
+		stateBox.getItems().add("IN");
+		stateBox.getItems().add("KS");
+		stateBox.getItems().add("KY");
+		stateBox.getItems().add("LA");
+		stateBox.getItems().add("MA");
+		stateBox.getItems().add("MD");
+		stateBox.getItems().add("ME");
+		stateBox.getItems().add("MI");
+		stateBox.getItems().add("MN");
+		stateBox.getItems().add("MO");
+		stateBox.getItems().add("MS");
+		stateBox.getItems().add("MT");
+		stateBox.getItems().add("NC");
+		stateBox.getItems().add("ND");
+		stateBox.getItems().add("NE");
+		stateBox.getItems().add("NH");
+		stateBox.getItems().add("NJ");
+		stateBox.getItems().add("NM");
+		stateBox.getItems().add("NV");
+		stateBox.getItems().add("NY");
+		stateBox.getItems().add("OH");
+		stateBox.getItems().add("OK");
+		stateBox.getItems().add("OR");
+		stateBox.getItems().add("PA");
+		stateBox.getItems().add("RI");
+		stateBox.getItems().add("SC");
+		stateBox.getItems().add("SD");
+		stateBox.getItems().add("TN");
+		stateBox.getItems().add("TX");
+		stateBox.getItems().add("UT");
+		stateBox.getItems().add("VA");
+		stateBox.getItems().add("VT");
+		stateBox.getItems().add("WA");
+		stateBox.getItems().add("WI");
+		stateBox.getItems().add("WV");
+		stateBox.getItems().add("WY");
 	}
 }
