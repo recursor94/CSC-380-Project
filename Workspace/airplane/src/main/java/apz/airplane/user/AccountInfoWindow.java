6df0a587de49e917c178a7126050c0ae7efff1bb
Nguyen <9phuc.nguyen6@gmail.com>
2018-04-21 16:28:28 -0400

    accountinfo window almost done. Needs secondary window and buttons to reference it

--- Workspace/airplane/src/main/java/apz/airplane/user/AccountInfoWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/AccountInfoWindow.java
@@ -1,10 +1,13 @@
 package apz.airplane.user;
 
 import apz.airplane.model.User;
+import apz.airplane.util.FilePath;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -15,10 +18,11 @@ public class AccountInfoWindow implements WindowInterface {
 
 	// have email as part of registration?
 
+	private ImageView img;
 	private VBox mainPane;
 	private GridPane gridPane;
 	private Text headerText;
-	private Button changeButton;
+	private Button changeButton, deleteButton;
 
 	private User uc;
 
@@ -30,36 +34,60 @@ public class AccountInfoWindow implements WindowInterface {
 	}
 
 	public void initialize() {
+		img = new ImageView(new Image(FilePath.LOGIN_IMAGE));
 		mainPane = new VBox(10);
 		gridPane = new GridPane();
 		headerText = new Text("Account Information");
 		changeButton = new Button("Change Information");
+		deleteButton = new Button("Delete Account");
 	}
 
 	public void content() {
 		uc = APZLauncher.getCurrentUser();
 
+		
+		gridPane.add(new ImageView(new Image(FilePath.LOGIN_IMAGE)), 1, 2);
 		gridPane.add(new Label("Email: "), 2, 2);
 		gridPane.add(new Label(uc.getEmail()), 3, 2);
 		
+		gridPane.add(new ImageView(new Image(FilePath.LOGIN_IMAGE)), 1, 3);
 		gridPane.add(new Label("Username: "), 2, 3);
 		gridPane.add(new Label(uc.getUsername()), 3, 3);
 
+		gridPane.add(new ImageView(new Image(FilePath.LOGIN_IMAGE)), 1, 4);
 		gridPane.add(new Label("Password: "), 2, 4);
 		gridPane.add(new Label("******"), 3, 4);
-		gridPane.add(changeButton, 4, 6);
+		gridPane.add(changeButton, 3, 6);
+		gridPane.add(deleteButton, 4, 6);
 		
-		mainPane.getChildren().addAll(headerText, new Separator(), gridPane);
+		mainPane.getChildren().addAll(new Label(), img, headerText, new Separator(), gridPane, new Label(), new Separator());
 	}
 	
 	public void actionEvents() {
+		
+		for (int i = 0; i < gridPane.getChildren().size(); i++) 
+			if (gridPane.getChildren().get(i) instanceof ImageView) {
+				((ImageView) gridPane.getChildren().get(i)).setFitWidth(50);
+				((ImageView) gridPane.getChildren().get(i)).setFitHeight(50);
+		}
+		
+		changeButton.setOnAction(event -> {
+			
+		});
+		
+		deleteButton.setOnAction(event -> {
+			// are you sure?
+			// if confirm, delete, go back to login screen
+		});
+	}
+
+	public void properties() {
 		headerText.setFont(new Font(32));
 		mainPane.setAlignment(Pos.TOP_CENTER);
 		gridPane.setVgap(10);
 		gridPane.setHgap(10);
-	}
-
-	public void properties() {
+		img.setFitWidth(150);
+		img.setFitHeight(150);
 		APZLauncher.getBorderPane().setCenter(mainPane);
 	}
 
