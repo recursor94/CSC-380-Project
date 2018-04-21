ea907cac453580c1bce31bacecc587e3bfc192fc
Andrew Spano <spana75@mai.sunysuffolk.edu>
2018-04-21 12:50:11 -0400

    reformatted cancel trip with a table

--- Workspace/airplane/src/main/java/apz/airplane/user/CancelFlightWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/CancelFlightWindow.java
@@ -1,6 +1,7 @@
 package apz.airplane.user;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import apz.airplane.model.Airplane;
@@ -19,11 +20,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class CancelFlightWindow {
 
-	private ListView<Booking> list;
+	private Text windowHeader;
+	private TableView<BookingInformation> tripTable;
+	private TableColumn<BookingInformation, Integer> flightNumberColumn;
+	private TableColumn<BookingInformation, String> arriveTimeColumn, departureTimeColumn, airlineColumn;
+	private TableColumn<BookingInformation, Airport> departureAirportColumn, destinationAirportColumn;
+	private TableColumn<BookingInformation, LocalDate> departureDateColumn, bookDateColumn;
+	private TableColumn<BookingInformation, Double> tripCostColumn;
 	private static VBox mainPane;
 	private Button confirmButton;
 	private User user;
@@ -38,62 +50,108 @@ public class CancelFlightWindow {
 	private void initialize() {
 		mainPane = new VBox(10);
 
-		list = new ListView<>();
+		tripTable = new TableView<>();
 		confirmButton = new Button("Cancel Flight");
+		windowHeader = new Text("Manage Trips");
+		flightNumberColumn = new TableColumn<>("Flight");
+		arriveTimeColumn = new TableColumn<>("Boarding");
+		departureTimeColumn = new TableColumn<>("Departing");
+		airlineColumn = new TableColumn<>("Airline");
+		departureAirportColumn = new TableColumn<>("From");
+		destinationAirportColumn = new TableColumn<>("Arriving");
+		departureDateColumn = new TableColumn<>("Date");
+		bookDateColumn = new TableColumn<>("Booked On");
+		tripCostColumn = new TableColumn<>("Subtotal");
 	}
 
 	private void content() {
 		user = APZLauncher.getCurrentUser();
-		ObservableList<Booking> displayList = FXCollections.observableArrayList(user.getTripList());
-		list.setItems(displayList);
-
+		resetTableData();
+		setupTableContents();
 		mainPane.setAlignment(Pos.CENTER);
-		mainPane.getChildren().addAll(new Label("List of Flights you made"), list, confirmButton);
-		
+		mainPane.getChildren().addAll(new Label("List of Flights you made"), tripTable, confirmButton);
+
 	}
+
 	private void properties() {
 		APZLauncher.getBorderPane().setCenter(mainPane);
+		formatHeader();
+	}
+
+	private void formatHeader() {
+		windowHeader.setStyle("-fx-font: 24 arial;");
+		windowHeader.setTextAlignment(TextAlignment.CENTER);
 	}
 
 	private void actionEvents() {
 		confirmButton.setOnAction(event -> {
-			System.out.println(list.getSelectionModel().getSelectedItem());
+			System.out.println(tripTable.getSelectionModel().getSelectedItem());
 
-			if (list.getSelectionModel().getSelectedItem() != null) {
-				Booking foundBooking = user.findTrip(list.getSelectionModel().getSelectedItem().getFlight());
+			if (tripTable.getSelectionModel().getSelectedItem() != null) {
+				Booking foundBooking = user.findTrip(tripTable.getSelectionModel().getSelectedItem().getFlight());
 
 				Optional<ButtonType> result = MessageBox.message(AlertType.CONFIRMATION, "APZ Confirmation Dialog",
 						"Are you okay with removing the selected flight?");
 				if (result.get() == ButtonType.OK) {
-					user.removeTrip(foundBooking.getFlight());
-					ObservableList<Booking> tripList = FXCollections.observableArrayList(user.getTripList());
-					list.setItems(tripList);
+					user.removeTrip(foundBooking.getFlight()); // Andrew's Note: If each trip is represented by a
+																// booking, why does
+					// removeTrip take Flight?
+
+					// ObservableList<Booking> tripList =
+					// FXCollections.observableArrayList(user.getTripList());
+					resetTableData();
 					APZState.saveInformation();
 
 				} else {
 					System.out.println(":(");
 				}
-			}
-			else {
+			} else {
 				MessageBox.message(AlertType.ERROR, "ERROR", "You must select a flight to cancel");
 			}
 
 		});
 	}
 
-//	public void createdStuff() {
-//		Airplane airplane = new Airplane(1, "American Airline", 5);
-//		LocalDate bookDate = LocalDate.now();
-//		LocalDate departure = LocalDate.of(2018, 3, 12);
-//		LocalDate arrival = LocalDate.of(2018, 3, 12);
-//		Time departTime = new Time("12:00 PM");
-//		Time arriveTime = new Time("7:00 PM");
-//		Airport departureAirport = new Airport("JFK", "New York");
-//		Airport destinationAirport = new Airport("Syracuse Hancock International", "Syracuse");
-//		Flight flight = new Flight(airplane, departureAirport, destinationAirport, arrival, departure, departTime, arriveTime, 40908);
-//		Booking trip = new Booking(flight, bookDate, user);
-//		user.addTrip(trip);
-//
-//	}
+	private void setupTableContents() {
+		System.out.println(user.getTripList().size());
+		flightNumberColumn.setCellValueFactory(new PropertyValueFactory<>("flightNumber"));
+		airlineColumn.setCellValueFactory(new PropertyValueFactory<>("Airline"));
+		arriveTimeColumn.setCellValueFactory(new PropertyValueFactory<>("arrivalTime"));
+		departureDateColumn.setCellValueFactory(new PropertyValueFactory<>("bookDate"));
+		departureTimeColumn.setCellValueFactory(new PropertyValueFactory<>("departureTime"));
+		departureAirportColumn.setCellValueFactory(new PropertyValueFactory<>("departureAirport"));
+		destinationAirportColumn.setCellValueFactory(new PropertyValueFactory<>("destinationAirport"));
+		bookDateColumn.setCellValueFactory(new PropertyValueFactory<>("bookDate"));
+		tripCostColumn.setCellValueFactory(new PropertyValueFactory<>("tripCost"));
+		tripTable.getColumns().addAll(flightNumberColumn, airlineColumn, departureDateColumn, arriveTimeColumn,
+				departureTimeColumn, departureAirportColumn, destinationAirportColumn, bookDateColumn, tripCostColumn);
+	}
+
+	private void resetTableData() {
+		ArrayList<Booking> tripList = user.getTripList();
+		ArrayList<BookingInformation> informationList = new ArrayList<>();
+		for (Booking trip : tripList) {
+			informationList.add(new BookingInformation(trip));
+		}
+		tripTable.setItems(FXCollections.observableArrayList(informationList));
+
+	}
+
+	// public void createdStuff() {
+	// Airplane airplane = new Airplane(1, "American Airline", 5);
+	// LocalDate bookDate = LocalDate.now();
+	// LocalDate departure = LocalDate.of(2018, 3, 12);
+	// LocalDate arrival = LocalDate.of(2018, 3, 12);
+	// Time departTime = new Time("12:00 PM");
+	// Time arriveTime = new Time("7:00 PM");
+	// Airport departureAirport = new Airport("JFK", "New York");
+	// Airport destinationAirport = new Airport("Syracuse Hancock International",
+	// "Syracuse");
+	// Flight flight = new Flight(airplane, departureAirport, destinationAirport,
+	// arrival, departure, departTime, arriveTime, 40908);
+	// Booking trip = new Booking(flight, bookDate, user);
+	// user.addTrip(trip);
+	//
+	// }
 
 }
