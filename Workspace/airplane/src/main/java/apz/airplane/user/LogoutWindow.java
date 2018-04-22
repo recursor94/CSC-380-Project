d01fb2c160248b77d8a34e96cd6387d15a42a9f6
zgudlin <timberwolves77@gmail.com>
2018-04-22 17:03:59 -0400

    I made a simple logout window

--- Workspace/airplane/src/main/java/apz/airplane/user/LogoutWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/LogoutWindow.java
@@ -3,11 +3,8 @@ package apz.airplane.user;
 import java.util.Optional;
 
 import apz.airplane.util.MessageBox;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonBar.ButtonData;
-import jimmy.pack.PasswordPromptWindow;
+import javafx.scene.control.ButtonType;
 
 public class LogoutWindow {
 	
@@ -17,17 +14,8 @@ public class LogoutWindow {
 	
 	private void confirmLogout() {
 		
-		Optional<ButtonType> result = MessageBox.message(AlertType.CONFIRMATION, "APZ Application - Logout", "Are you sure you want to logout?");
-//		Alert alert = new Alert(AlertType.CONFIRMATION);
-//		alert.setTitle("APZ Application - Logout");
-//		alert.setHeaderText(null);
-//		alert.setContentText("Are you sure you want to logout?");
-//
-//		ButtonType yesButton = new ButtonType("Yes");
-//
-//		alert.getButtonTypes().setAll(yesButton, new ButtonType("No", ButtonData.CANCEL_CLOSE));
-//		
-//		Optional<ButtonType> result = alert.showAndWait();
+		Optional<ButtonType> result = MessageBox.message(AlertType.CONFIRMATION, "APZ Application - Logout", "Press OK to confirm logout");
+
 		if (result.get() == ButtonType.OK) {
 			new LoginWindow();
 			APZLauncher.getBorderPane().setTop(null);
