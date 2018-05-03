49850953be0fea3d55eaae1a2a8b7d2600c2e909
Andrew Spano <spana75@mai.sunysuffolk.edu>
2018-05-03 12:33:05 -0400

    removed clutter from home

--- Workspace/airplane/src/main/java/apz/airplane/user/gui/home/HomeScreenWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/gui/home/HomeScreenWindow.java
@@ -1,11 +1,7 @@
 package apz.airplane.user.gui.home;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import apz.airplane.admin.AdminState;
 import apz.airplane.model.Flight;
@@ -13,9 +9,6 @@ import apz.airplane.user.gui.APZLauncher;
 import apz.airplane.util.APZState;
 import apz.airplane.util.FilePath;
 import apz.airplane.util.GuiApplication;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
@@ -25,17 +18,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 public class HomeScreenWindow implements GuiApplication {
 	private ImageView img;
 	private VBox rootPane;
-	private Text timeLabel, dateLabel, header, newsHeader, newsUpdate;
-	private Timeline realTimeClock;
+	private Text header, newsHeader, newsUpdate;
 	private ArrayList<HomeTableData> flightsToday;
-
-	private int timeHour;
-	private int timeMinute;
 
 	public HomeScreenWindow() {
 		initialize();
@@ -45,7 +33,7 @@ public class HomeScreenWindow implements GuiApplication {
 	}
 
 	public void actionEvents() {
-		
+
 	}
 
 	public void properties() {
@@ -60,67 +48,25 @@ public class HomeScreenWindow implements GuiApplication {
 		flightsToday = getFlightsToday();
 		orderFlightsByTime();
 		// timeLabel.setText(LocalDateTime.now().toString());
-		timeLabel.setStyle("-fx-font: 24 arial;");
-		timeLabel.setFill(Color.BLACK);
-		dateLabel.setText(LocalDateTime.now().toString());
-		dateLabel.setStyle("-fx-font: 24 arial;");
-		dateLabel.setFill(Color.BLACK);
 		newsHeader.setStyle("-fx-font: 22 arial;");
 		newsHeader.setFill(Color.BLACK);
 		newsUpdate.setStyle("-fx-font: 20 arial;");
 		newsUpdate.setFill(Color.DARKSLATEGRAY);
-
-		header.setFont(new Font(28));
-		rootPane.getChildren().addAll(new Label(), img, header, new Separator(), dateLabel, timeLabel, new Separator(), newsHeader, newsUpdate);
+		header.setStyle("-fx-font: 38 arial;");
+		rootPane.getChildren().addAll(new Label(), img, header, new Separator(),
+				newsHeader, newsUpdate);
 		rootPane.setAlignment(Pos.TOP_CENTER);
 		APZLauncher.getBorderPane().setCenter(rootPane);
 		APZLauncher.getStage().setTitle("APZ Application - Home Screen");
-		refreshClockContent();
-		setupClock();
-		realTimeClock.stop();
-		realTimeClock.playFromStart();
-
 	}
 
 	public void initialize() {
 		img = new ImageView(new Image(FilePath.HOME_PLANE));
 		flightsToday = new ArrayList<>();
 		rootPane = new VBox(10);
-		dateLabel = new Text("");
-		timeLabel = new Text("00:00");
-		header = new Text("");								// wahat?			JUST HAVE NEWS. OTHER STUFF IS CLUTTER
 		newsUpdate = new Text(AdminState.readUpdate());
 		newsHeader = new Text("Today's News:\n");
-		setupClock();
-	}
-
-	private void setupClock() {
-		//System.out.println("Init clock");
-		realTimeClock = new Timeline(new KeyFrame(Duration.minutes(1), e -> {
-			refreshClockContent();
-		}));
-		realTimeClock.setCycleCount(Animation.INDEFINITE);
-
-	}
-
-	private void refreshClockContent() {
-		Calendar cal = Calendar.getInstance();
-		timeHour = cal.get(Calendar.HOUR);
-		timeMinute = cal.get(Calendar.MINUTE);
-		// timeHour++;
-		String timeOfDay = "AM";
-
-		if (cal.get(Calendar.AM_PM) == Calendar.PM) {
-			timeOfDay = "PM";
-		}
-		DateFormat dateFormat = new SimpleDateFormat("EEEE, MMMMM dd");
-		DateFormat timeFormat = new SimpleDateFormat("hh:mm");
-		String sDate = dateFormat.format(cal.getTime());
-		String sTime = timeFormat.format(cal.getTime());
-		// timeLabel.setText(timeHour + ":" + (minuteString) + " " + timeOfDay);
-		timeLabel.setText(sTime + " " + timeOfDay);
-		dateLabel.setText(sDate);
-
+		header = new Text ("Home");
 	}
 
 	private ArrayList<HomeTableData> getFlightsToday() {
