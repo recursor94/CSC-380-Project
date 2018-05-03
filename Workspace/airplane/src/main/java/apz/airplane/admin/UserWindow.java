6c35559d2ea3dbcfdac5f4918d58d0c2ce1b2059
zgudlin <timberwolves77@gmail.com>
2018-05-03 12:59:07 -0400

    Divided the email check into two cases in UserWindow

--- Workspace/airplane/src/main/java/apz/airplane/admin/UserWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/admin/UserWindow.java
@@ -133,8 +133,10 @@ public class UserWindow {
 				else if (password.contains(" "))
 					MessageBox.message(AlertType.ERROR, "Invalid Password",
 							"Your password cannot contain the empty space character");
-				else if (!emailField.getText().contains("@") && !emailField.getText().contains("."))
-					MessageBox.message(AlertType.ERROR, "Invalid Email", "Your email must be properly formatted!");
+				else if (!emailField.getText().contains("@"))
+					MessageBox.message(AlertType.ERROR, "Invalid Email", "Your email must be properly formatted with a directory!");
+				else if (!emailField.getText().contains("."))
+					MessageBox.message(AlertType.ERROR, "Invalid Email", "Your email must be properly formatted with a domain!");
 				else {
 					uc.addUser(new User(email, username, password));
 					AdminState.saveInformation(uc);
