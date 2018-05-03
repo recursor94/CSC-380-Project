1b8ab4c0e434e48de7d35a7ad56d6f254f9a41f8
Andrew Spano <spana75@mai.sunysuffolk.edu>
2018-05-03 12:38:21 -0400

    Removed header from home window screen

--- Workspace/airplane/src/main/java/apz/airplane/user/gui/home/HomeScreenWindow.java
+++ Workspace/airplane/src/main/java/apz/airplane/user/gui/home/HomeScreenWindow.java
@@ -22,7 +22,7 @@ import javafx.scene.text.Text;
 public class HomeScreenWindow implements GuiApplication {
 	private ImageView img;
 	private VBox rootPane;
-	private Text header, newsHeader, newsUpdate;
+	private Text newsHeader, newsUpdate;
 	private ArrayList<HomeTableData> flightsToday;
 
 	public HomeScreenWindow() {
@@ -45,16 +45,11 @@ public class HomeScreenWindow implements GuiApplication {
 
 		img.setFitWidth(150);
 		img.setFitHeight(150);
-		flightsToday = getFlightsToday();
-		orderFlightsByTime();
-		// timeLabel.setText(LocalDateTime.now().toString());
 		newsHeader.setStyle("-fx-font: 22 arial;");
 		newsHeader.setFill(Color.BLACK);
 		newsUpdate.setStyle("-fx-font: 20 arial;");
 		newsUpdate.setFill(Color.DARKSLATEGRAY);
-		header.setStyle("-fx-font: 38 arial;");
-		rootPane.getChildren().addAll(new Label(), img, header, new Separator(),
-				newsHeader, newsUpdate);
+		rootPane.getChildren().addAll(new Label(), img, new Separator(), newsHeader, newsUpdate);
 		rootPane.setAlignment(Pos.TOP_CENTER);
 		APZLauncher.getBorderPane().setCenter(rootPane);
 		APZLauncher.getStage().setTitle("APZ Application - Home Screen");
@@ -66,37 +61,5 @@ public class HomeScreenWindow implements GuiApplication {
 		rootPane = new VBox(10);
 		newsUpdate = new Text(AdminState.readUpdate());
 		newsHeader = new Text("Today's News:\n");
-		header = new Text ("Home");
-	}
-
-	private ArrayList<HomeTableData> getFlightsToday() {
-		ArrayList<Flight> allFlights = APZState.loadFlights();
-		ArrayList<HomeTableData> flightsToday = new ArrayList<>(); // has to be new arraylist
-		for (Flight flight : allFlights) {
-			if (flight.getArriveDate().isEqual(LocalDate.now())) {
-				flightsToday.add(new HomeTableData(flight.getFlightNum(), flight.getDepartureAirport().toString(),
-						flight.getDestinationAirport().toString(), flight.getDepartureTime().getTimeDouble(),
-						flight.getDepartureTime().getTimeString(), flight));
-			}
-		}
-		return flightsToday;
-	}
-
-	private void orderFlightsByTime() {
-		HomeTableData temp, previous;
-
-		for (int i = 0; i < flightsToday.size(); i++) {
-			previous = flightsToday.get(i);
-
-			for (int j = 0; j < flightsToday.size(); j++) {
-				double previousTime = previous.getTime();
-				double comparisonTime = flightsToday.get(j).getTime();
-				if (comparisonTime < previousTime) {
-					temp = flightsToday.get(i);
-					flightsToday.set(i, flightsToday.get(j));
-					flightsToday.set(j, temp);
-				}
-			}
-		}
 	}
 }
