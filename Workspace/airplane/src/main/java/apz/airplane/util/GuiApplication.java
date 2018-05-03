ff6f339e10ddcebdc52345b3f5e163dca11433f1
Nguyen <9phuc.nguyen6@gmail.com>
2018-05-03 14:48:08 -0400

    ppt update

--- Workspace/airplane/src/main/java/apz/airplane/util/GuiApplication.java
+++ Workspace/airplane/src/main/java/apz/airplane/util/GuiApplication.java
@@ -1,10 +1,10 @@
 package apz.airplane.util;
 
 public interface GuiApplication {
-	
 	public void initialize();
 	public void content();
 	public void actionEvents();
 	public void properties();
-	
 }
+
+
